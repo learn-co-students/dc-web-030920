0cd9c434a93ec082f8ef42462fd7d1be8931c361
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2020-04-21 15:02:01 -0400

    added 34

--- 34-JS-Events/README.md
+++ 34-JS-Events/README.md
@@ -1,18 +1,37 @@
 # Event Listeners
+## Type of Events
+- submit
+- click
+- keyboard events (keypress)
+- mouseover
+- DOMContentLoaded
+
+
+//When some event happens => DOM changes
+ // a new row for shannon the table
+ // clear the form
+
+
 ## .addEventListener() on a DOM node
-- takes in a string for the name of the event, and a callback function to be invoked when the event happens
+- takes in a string for the type of the event, and a callback function to be invoked when the event happens
 - (optional) argument for the callback is the event itself
 
-## Example of Events
+
+## Practice
+- When submit the form -> add new instructor row
+- When click of email -> alert("contacting <email> ")
 
 
 ## When to add an event Listener
 - on load of page?
 - on creation of element?
 
-## Practice
-- When click of email -> alert("sent an email")
-- When click of submit -> add new instructor
+
 
 ## Some events have default behavior
 - event.preventDefault()
+
+
+//DOM
+//Events
+//Server Communication (saving to the Database)
--- 34-JS-Events/script.js
+++ 34-JS-Events/script.js
@@ -1,47 +1,98 @@
-console.log("first line")
-setTimeout(function(){console.log("hello right away")}, 1000)
-console.log("hello from bottom of the file")
+//anytime to need to depend on the DOM
+document.addEventListener("DOMContentLoaded", () => {
+  let formNode = document.querySelector("form")
+  //1. Have the form listen for a submit event
+  formNode.addEventListener("submit", handleForm)
 
+  //When the email td is click => alert
+  //3 rows
+  let emailNodes = document.querySelectorAll(".email")
+  for(let i=0; i<emailNodes.length; i++){
+    emailNodes[i].addEventListener("click", handleEmail)
+  }
 
-//document.addEventListener("DOMContentLoaded", function(){
-//
-//})
+})
 
+function handleEmail(event){
+  alert("sending email to " + event.target.innerText)
+}
 
+function handleForm(event){
+  event.preventDefault()
 
+  //2. add a new row to tbody
+  let first = document.getElementById("firstNameInput").value
+  let last = document.getElementById("lastNameInput").value
+  let email = document.getElementById("emailInput").value
 
+  addInstructor(first, last, email)
 
-
-
-
-
-
-
+  //3. Reset the form
+  let formNode = document.querySelector("form")
+  formNode.reset()
+  //event.target.reset()
+}
 
 function addInstructor(firstName, lastName, email){
+  //creating my <tr>
+  var trElement = document.createElement("tr")
   var tBodyEl = document.querySelector("tbody")
-  tBodyEl.innerHTML += `<tr>
-    <td class="firstName">${firstName}</td>
-    <td class="lastName">${lastName}</td>
-    <td class="email">${email}</td>
-  </tr>`
+  tBodyEl.appendChild(trElement) //appending it to the <tbody>
+
+  var firstNameEl = document.createElement("td")//creating the <td>
+  firstNameEl.innerText = firstName//updating the <td> to have a innerText of Jane
+
+  var lastNameEl = document.createElement("td")//creating the <td>
+  lastNameEl.innerText = lastName//updating the <td> to have a innerText of Jane
+
+  var emailEl = document.createElement("td")//creating the <td>
+  emailEl.innerText = email//updating the <td> to have a innerText of Jane
+  emailEl.addEventListener("click", handleEmail)
+
+  trElement.append(firstNameEl, lastNameEl, emailEl)//appending to the tbody
 }
+
+//When some event happens => change the DOM
+//1. listen for the event addEventListener
+//2. change the DOM  
+
+
 
 
 // function addInstructor(firstName, lastName, email){
-//   //creating my <tr>
-//   var trElement = document.createElement("tr")
-//   var tBodyEl = document.querySelector("tbody")
-//   tBodyEl.appendChild(trElement) //appending it to the <tbody>
-//
-//   var firstNameEl = document.createElement("td")//creating the <td>
-//   firstNameEl.innerText = firstName//updating the <td> to have a innerText of Jane
-//
-//   var lastNameEl = document.createElement("td")//creating the <td>
-//   lastNameEl.innerText = lastName//updating the <td> to have a innerText of Jane
-//
-//   var emailEl = document.createElement("td")//creating the <td>
-//   emailEl.innerText = email//updating the <td> to have a innerText of Jane
-//
-//   trElement.append(firstNameEl, lastNameEl, emailEl)//appending to the tbody
+//   let tBodyNode = document.querySelector("tbody")
+//   tBodyNode.innerHTML += `<tr>
+//     <td class="firstName">${firstName}</td>
+//     <td class="lastName">${lastName}</td>
+//     <td class="email">${email}</td>
+//   </tr>`
 // }
+
+
+
+
+
+
+
+
+
+//
+//
+// function timesTwo(num){ //num = 1; num = 2; num = 3
+//   return num * 2
+// }
+//
+// [1, 2, 3, 4].map(timesTwo)
+//
+
+
+
+
+
+
+
+
+
+
+
+//
